fc56a08317aa9812fb0fe269e0bc29d12db83f54
shirley1108 <144615045+shirley1108@users.noreply.github.com>
2025-03-27 00:48:32 -0400

    Add files via upload

--- index.sql
+++ index.sql
@@ -1,5 +1,6 @@
---Index on Customer_ID for Faster Booking Lookups
---Index on Renting (Start_Date), Renting(End_Date) for Faster Availability Checks
+--Index on Customer_ID for Faster Booking Lookups.This index helps in queries that search for bookings by a specific customer.
+--Index on Renting (Start_Date), Renting(End_Date) for Faster Availability Checks.
+--When checking if a room is available, we often query for overlapping dates.
 CREATE INDEX idx_booking_customer_id ON booking (customer_ID);
 CREATE INDEX idx_renting_start_dates ON renting (start_Date);
 CREATE INDEX idx_renting_end_dates ON renting (end_Date);
